24f02775faa13ee551891ed9052f7de0026445db
Jaydeng75 <133883890+Jaydeng75@users.noreply.github.com>
2026-03-04 00:02:34 +0530

    Implement MyThread class with run method

---
+++ Thread.java
@@ -0,0 +1,17 @@
+class MyThread extends Thread {
+
+    @Override
+    public void run() {
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("Child Thread: " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread t = new MyThread();
+        t.start();
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("Main Thread: " + i);
+        }
+    }
+}
